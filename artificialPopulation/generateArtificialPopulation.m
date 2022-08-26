af0285b9bab1cb26fb00e719b2324b3f2283b996
Hayden Scott <43681457+Hscott5@users.noreply.github.com>
2022-08-26 13:12:36 -0400

    Add files via upload

--- artificialPopulation/generateArtificialPopulation.m
+++ artificialPopulation/generateArtificialPopulation.m
@@ -6,41 +6,41 @@ function [pref, tuning, groundTruth] = generateArtificialPopulation(varargin)
 p = inputParser;
 defaultDims = 128;
 addParameter(p, 'nDims', defaultDims, @isnumeric);
-addParameter(p, 'distrobution', 'uniformGaussian');
-addParameter(p, 'tuningSigma', [])
+addParameter(p, 'distribution', 'uniformGaussian');
+addParameter(p, 'tuningWidth', 50)
 addOptional(p, 'nNeurons', 100);
-addOptional(p, 'nInvariantDims', 5);
+addOptional(p, 'nInvariantDims', 0);
 parse(p, varargin{:});
 nDims = p.Results.nDims;
 N = p.Results.nNeurons;
 nInvariantDims = p.Results.nInvariantDims;
-% if isempty(p.Results.tuningSigma)
-%     tuningSigma = 3;
-% else
-%     tuningSigma = p.Results.tuningSigma;
-% end
+tuneWidth = p.Results.tuningWidth;
 
 %% Actual Function
 
 pref = normc(randn(nDims,N)).*sqrt(nDims);
 
-switch p.Results.distrobution
+switch p.Results.distribution
     case 'uniformGaussian'
-        tuning = repmat(eye(nDims),1,1,N)./nDims;
+        tuning = repmat(eye(nDims),1,1,N)./tuneWidth;
     case 'randInvariant'
-        tuning = repmat(eye(nDims),1,1,N)./nDims;
+        tuning = repmat(eye(nDims),1,1,N)./tuneWidth;
         for t = 1:N
             x = randperm(nDims, nInvariantDims);
             tuning(x,:,t) = 0;
         end
     case 'randVariant'
-        tuning = repmat(eye(nDims),1,1,N)./nDims;
+        tuning = repmat(eye(nDims),1,1,N)./tuneWidth;
         for t = 1:N
             x = randperm(nDims, nInvariantDims);
             tuning(x,:,t) = rand(nInvariantDims,1).*tuning(x,:,t)*2;
         end
     case 'groundTruth'
         groundTruth = normc(randn(nDims,1)).*sqrt(nDims);
-        tuning = repmat(eye(nDims),1,1,N)./(nDims/1.5);
-        pref = randn(nDims,N)+groundTruth;
+        tuning = repmat(eye(nDims),1,1,N)./(tuneWidth);
+        if nInvariantDims>0
+            inv = randperm(nDims,nInvariantDims);
+            tuning(inv, :,:)=0;
+        end
+        pref = randn(nDims,N) + groundTruth;
 end
