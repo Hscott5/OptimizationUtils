25beb0aabf353d8042327a34394bd1c95373b6bd
Hayden Scott <43681457+Hscott5@users.noreply.github.com>
2022-08-26 12:40:54 -0400

    Delete Optimization Utils directory

--- Optimization Utils/adaptiveRobot/adeptResponse.m
+++
@@ -1,9 +0,0 @@
-function response = adeptResponse(stims, latents, responses)
-
-
-stims = stims';
-dists = pdist2(latents,stims);
-[closestIndx, ~] = find(dists==min(dists));
-response = responses(closestIndx,:)';
-
-
--- Optimization Utils/particleSwarm/plotResponseNorms.m
+++
@@ -1,9 +0,0 @@
-function plotResponseNorms(history)
-
-responses = history.response;
-norms = cellfun(@(x) sqrt(sum(x.^2,1)), responses, 'UniformOutput', false);
-norms = cat(1,norms{:});
-figure
-plot(norms);
-xlabel('Generation');
-ylabel('Response Norm');
